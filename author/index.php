05a406aecebae2811586f44d8fa5efeda66179f8
Julien <julien_max.garb@fh-bielefeld.de>
2021-05-05 13:15:26 +0200

    added header for cors

--- author/index.php
+++ author/index.php
@@ -23,6 +23,7 @@ class GetAuthorsRoute{
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
   $API = new GetAuthorsRoute;
+  header("Access-Control-Allow-Origin: http://localhost:3000");
   header('Content-Type: application/json');
   echo $API->getAllAuthors();
 }
